ca51ac866257d5455d7e83bcd6dcf092e771ba1f
57203BRO <57203BRO@RESU-216-08.wcdsb.ca>
2024-12-02 10:13:17 -0500

    For Testing

--- src/BlackJack/Player.java
+++ src/BlackJack/Player.java
@@ -1,5 +1,45 @@
 package BlackJack;
 
+import java.util.ArrayList;
+
 public class Player {
-		
-}
+	private ArrayList<Integer> cards = new ArrayList<Integer>();
+	public Player(int[] cards) 
+	{
+		for(int i = 0;i<cards.length;i++) 
+		{
+			this.cards.add(cards[i]);
+		}
+	}
+	public void addcard(int NewCard) 
+	{
+		this.cards.add(NewCard);
+	}
+	public int getScore() 
+	{
+		int score = 0;
+		for(int i:this.cards) 
+		{
+			score+=i%13+1;
+		}
+		return score;
+	}
+	public int[] showHand() 
+	{
+		int[] cardArray = new int[this.cards.size()];
+		int j = 0;
+		for(int i:this.cards) 
+		{
+			cardArray[j] = i;
+			j+=1;
+		}
+		return cardArray;
+	}
+	public void clearHand() 
+	{
+		for(int i=0; i<this.cards.size();i++) 
+		{
+			this.cards.remove(0);
+		}
+	}
+}	
