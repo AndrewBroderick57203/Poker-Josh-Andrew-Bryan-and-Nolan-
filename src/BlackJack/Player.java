97eab766f5ad99905f8be837b2df43cd7e7a7a61
57203BRO <57203BRO@RESU-216-08.wcdsb.ca>
2024-12-03 09:49:31 -0500

    The player class is complete, life is pointless, the project is worthless

--- src/BlackJack/Player.java
+++ src/BlackJack/Player.java
@@ -3,7 +3,7 @@ package BlackJack;
 import java.util.ArrayList;
 
 public class Player {
-	//
+	//cards array for the player
 	private ArrayList<Integer> cards = new ArrayList<Integer>();
 	/**Defines player
 	 * @param cards The players cards
@@ -31,19 +31,50 @@ public class Player {
 	{
 		//Calculation stuff
 		int score = 0;
+		int aces = 0;
+		// Handling most cards
 		for(int i:this.cards) 
 		{
-			score+=i%13+1;
+			//Ace
+			if(i%13 == 0) 
+			{
+				//Save it for later
+				aces += 1;
+			}
+			//Face Card
+			else if(i%13>9) 
+			{
+				score+=10;
+			}
+			//Number Card
+			else
+			{
+				score+=i%13+1;
+			}
+		}
+		//Ace Handling as they have 2 possible values and thus must be dealt with last
+		for(int i = 0;i<aces;i++) 
+		{
+			//Making the aces get the player as close as possible to 21 but below or equal
+			if(score<12-aces) 
+			{
+				score+=11;
+			}
+			else 
+			{
+				score+=1;
+			}
 		}
 		//Returns score
 		return score;
 	}// End getScore()
 	
-	/**
-	 * @return
+	/**Returns an array of the current cards
+	 * @return array of the size of the # of cards
 	 */
 	public int[] showHand() 
 	{
+		//Makes int array and fills them with the cards
 		int[] cardArray = new int[this.cards.size()];
 		int j = 0;
 		for(int i:this.cards) 
@@ -51,14 +82,16 @@ public class Player {
 			cardArray[j] = i;
 			j+=1;
 		}
+		//Returns int array of da cards!!!!!!!
 		return cardArray;
 	}//End showHand()
 	
 	/**
-	 * 
+	 * Empties the players hand
 	 */
 	public void clearHand() 
 	{
+		//Deletes ALL OF THE CARDS!
 		for(int i=0; i<this.cards.size();i++) 
 		{
 			this.cards.remove(0);
