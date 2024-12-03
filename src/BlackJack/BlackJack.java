09cf17fa8092e9cca29255d5093dd3c4e535fed4
57203BRO <57203BRO@RESU-216-08.wcdsb.ca>
2024-12-03 10:07:16 -0500

    I want to kill myself

---
+++ src/BlackJack/BlackJack.java
@@ -0,0 +1,60 @@
+package BlackJack;
+
+public class BlackJack {
+	public static void main(String[] args)
+	{
+		//initiate scanner
+		//create new Deck
+		//create new Player
+		//create new Dealer
+
+		// Initial dealing
+		//add 2 cards to player
+		//add 2 cards to dealer
+
+		// Player's turn
+		boolean playerBust = false;
+		//while (true) 
+		//{
+		//show player’s hand
+		//print player score
+
+		/*if (player score > 21) 
+		{
+			print “You win!”
+			playerBust = true
+			break
+		}*/
+
+		/*print "Do you want to (h)it or (s)tand? "
+		String choice = scanner.nextLine()
+		if (choice = “h”) 
+		{
+			add card to player from deck;
+		} else 
+		{
+			break
+		}*/
+		//}
+
+		// Dealer's turn
+		/*if (player haven’t bust) 
+		{
+			while (dealer score < 17) 
+			{
+				add card to dealer
+			}
+
+			// Show results
+			show dealer hand
+			print dealer score
+
+			// Determine winner
+			if (dealer score > 21) 
+			{
+				print “Dealer bust! You win!”
+			}
+		}*/
+
+	}
+}
