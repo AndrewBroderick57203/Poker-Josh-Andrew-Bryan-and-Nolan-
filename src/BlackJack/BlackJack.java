f171f5e593281d1511df26adbfd5b33822531840
57203BRO <57203BRO@RESU-216-08.wcdsb.ca>
2024-12-06 09:57:01 -0500

    o_0

--- src/BlackJack/BlackJack.java
+++ src/BlackJack/BlackJack.java
@@ -7,8 +7,46 @@ public class BlackJack {
 	private String PrintDaCards(int[] cards) 
 	{
 		String cardsString = "";
-		for()
-		return "clubs (♣), diamonds (♦), hearts (♥), and spades (♠)";
+		for(int i = 0;i<cards.length;i++) 
+		{
+			if(Math.floor(cards[i]/13) == 0) 
+			{
+				cardsString += "♥";
+			}
+			else if(Math.floor(cards[i]/13) == 1) 
+			{
+				cardsString += "♦";
+			}
+			else if(Math.floor(cards[i]/13) == 2) 
+			{
+				cardsString += "♣";
+			}
+			else if(Math.floor(cards[i]/13) == 3) 
+			{
+				cardsString += "♠";
+			}
+			if(cards[i]%13 == 0) 
+			{
+				cardsString += "A";
+			}
+			if(cards[i]%13 == 10) 
+			{
+				cardsString += "J";
+			}
+			if(cards[i]%13 == 11) 
+			{
+				cardsString += "Q";
+			}
+			if(cards[i]%13 == 12) 
+			{
+				cardsString += "K";
+			}
+			else
+			{
+				cardsString += Integer.toString(cards[i]%13);
+			}
+		}
+		return cardsString;
 	}
 	public static void main(String[] args)
 	{
@@ -27,8 +65,6 @@ public class BlackJack {
 		boolean playing = true;
 		boolean playerBust = false;
 		while (playing) 
-		{
-		}
 			System.out.println(MyGuy.showHand());
 			System.out.println(MyGuy.getScore());
 
